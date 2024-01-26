f735ca84f6ce68966fc70e66b8c5611069eff30c
snacky101 <snacky101@gmail.com>
2024-01-26 21:55:50 +0900

    Set line number

--- lua/options.lua
+++ lua/options.lua
@@ -2,8 +2,13 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+
 vim.cmd("set ignorecase")
 vim.cmd("set smartcase")
+
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set cursorline")
 
 vim.g.mapleader = " "
 
